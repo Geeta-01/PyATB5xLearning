4a63328c61721430ecc4a76b44172c7bfe2c89df
Geeta-01 <ms.geeta3609@gmail.com>
2025-01-18 23:37:39 +0530

    selenium mini project- task-1(item name and price)

--- Jan/ex04_selenium_locator_xpath/Task_Selenium_miniProj.py
+++ Jan/ex04_selenium_locator_xpath/Task_Selenium_miniProj.py
@@ -19,8 +19,15 @@ def test_Get_title_price():
     driver.get("https://www.ebay.com/b/Desktops-All-In-One-Computers/171957/bn_1643067")
     #print("Title of the webpage:",driver.title)
     time.sleep(5)
+    #select the search and send "MacMini"
+    search_input = driver.find_element(By.XPATH,"//div[@id='gh-ac-wrap']/input")
+    search_input.send_keys("Macmini")
+    #click on search
+    search_button= driver.find_element(By.ID,"gh-search-btn")
+    search_button.click()
+
     # get total number of items in the webpage
-    ul_xpath = '//ul[@class="brwrvr__item-results brwrvr__item-results--list"]'
+    ul_xpath = '//ul[@class="srp-results srp-list clearfix"]'
     ul_element = driver.find_element(By.XPATH, ul_xpath)
     # Find all 'li' elements within the 'ul'
     li_elements = ul_element.find_elements(By.TAG_NAME, 'li')
@@ -28,17 +35,16 @@ def test_Get_title_price():
     total_li = len(li_elements)
     print(f"Total number of 'li' elements: {total_li}")
 
-    for i in range(1,60):
+    for i in range(1,total_li):
         #get xpath for the title of the item
-        xpath_1 = f"//ul/li[{i}]/div/div/div[2]/div[1]/div[1]/span/a"
+        xpath_1 = f"//ul/li[{i}]/div/div[2]/a"
         web_element_text = driver.find_element(By.XPATH, xpath_1)
         print(f" item-{i} text:{web_element_text.text}")
 
         # get xpath for the price of the item
-        xpath_2 = f"//ul/li[{i}]/div/div/div[2]/div[2]/div[1]/span"
-        web_element_price =driver.find_element(By.XPATH,xpath_2)
+        xpath_2 = f"//ul/li[{i}]/div/div[2]/div[3]/div/div/span"
+        web_element_price = driver.find_element(By.XPATH, xpath_2)
         print(f" item-{i} price:{web_element_price.text}")
-    #web_element.click()
-    time.sleep(5)
+    time.sleep(30)
     driver.close()
 
